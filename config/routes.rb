18f2c70b1f6212a97d86dbdcdb3922e96062dc01
ariel-waisburg <arielwaisburg@gmail.com>
2021-02-22 17:43:48 -0300

    basic routes added

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :offers, except: [ :destroy ] do
+    resources :bookings, only: [ :new, :create ]
+  end
+  resources :bookings , only: [ :index, :show ] do
+    resources :reviews, only: [ :create ]
+  end
 end
