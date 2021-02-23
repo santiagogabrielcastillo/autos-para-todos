2ad60d2ddd4fba038b2ba4e5ceeb8ebc03025f12
Santiago Castillo <santiagogabrielcastillo@hotmail.com>
2021-02-23 15:40:02 -0300

    adds new and create actions for automobiles controller

---
+++ app/controllers/automobiles_controller.rb
@@ -0,0 +1,22 @@
+class AutomobilesController < ApplicationController
+
+  def new
+    @automobile = Automobile.new
+  end
+
+  def create
+    @automobile = Automobile.new(automobile_params)
+    @automobile.user = current_user
+    if @automobile.save
+      redirect_to automobile_path(@automobile)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def automobile_params
+    params.require(:automobile).permit(:brand, :model, :year, :km, :category, :price, :photo)
+  end
+end
