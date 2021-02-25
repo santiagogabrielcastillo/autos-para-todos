d655319182c36bb3358d1e4bd081f4edd421a41a
Santiago Castillo <santiagogabrielcastillo@hotmail.com>
2021-02-25 12:45:57 -0300

    adds flatpicker but not working

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -25,10 +25,13 @@ require("channels")
 // External imports
 import "bootstrap";
 
+
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
+import { initFlatpickr } from "../plugins/flatpickr";
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
+  initFlatpickr();
 });
