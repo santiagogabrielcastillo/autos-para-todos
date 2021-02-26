2cf6061fd8f1a5b39793c4b52ec85c4c2ee6d5ed
ariel-waisburg <arielwaisburg@gmail.com>
2021-02-26 15:36:15 -0300

    map and automobiles grid

--- app/javascript/plugins/init_autocomplete.js
+++ app/javascript/plugins/init_autocomplete.js
@@ -1,7 +1,7 @@
 import places from 'places.js';
 
 const initAutocomplete = () => {
-  const addressInput = document.getElementById('location');
+  const addressInput = document.getElementById('flat_address');
   if (addressInput) {
     places({ container: addressInput });
   }
